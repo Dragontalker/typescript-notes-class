e6ec3653c5d2af1470e424e98c9b867f5ca8e4c1
Richard <richard.yang.tong@gmail.com>
2021-04-16 16:49:50 -0400

    See what protected class can do.

--- classes.ts
+++ classes.ts
@@ -1,5 +1,5 @@
 class Vehicle {
-  public honk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 }
@@ -10,7 +10,7 @@ class Car extends Vehicle {
   }
 
   startDrivingProcess(): void {
-    this.drive();
+    this.honk();
   }
 };
 
